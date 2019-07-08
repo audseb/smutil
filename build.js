47d76df70acd0b8aa0901db1a701c8b237fa34d0
James Sturges <james@jsturges.com>
2019-07-08 16:20:28 -0400

    adding server option

--- build.js
+++ build.js
@@ -3,6 +3,8 @@ const collections = require('metalsmith-collections')
 const layouts = require('metalsmith-layouts')
 const robots = require('metalsmith-robots')
 const sitemap = require('metalsmith-sitemap')
+const watch = require('metalsmith-watch')
+const metalsmithExpress = require('metalsmith-express')
 
 const DOMAIN = 'https://smutil.com'
 const SITEMAP = 'sitemap.xml'
@@ -10,8 +12,9 @@ const SITEMAP = 'sitemap.xml'
 const buildinfo = require('./plugins/buildinfo.js')
 const constants = require('./plugins/constants.js')
 
-Metalsmith(__dirname).
-  source('./src').
+const SRC_DIR = 'src'
+const M = Metalsmith(__dirname).
+  source(`./${SRC_DIR}`).
   destination('./build').
   clean(true).
   use(buildinfo()).
@@ -39,7 +42,24 @@ Metalsmith(__dirname).
   use(layouts({
     default: 'util.hbs',
     pattern: '**/*.html'
-  })).
-  build(function(err, files) {
-    if (err) throw err
-  })
+  }))
+
+if (process.env.SERVE) {
+  const host = 'localhost'
+  const port = 3003
+
+  console.log(`  >> serving content on ${host}:${port}`)
+  M.use(metalsmithExpress({
+    host,
+    port
+  })).use(watch({
+    livereload: true,
+    paths: {
+      [`${SRC_DIR}/**/*`]: true
+    }
+  }))
+}
+
+M.build(function(err, files) {
+  if (err) throw err
+})
