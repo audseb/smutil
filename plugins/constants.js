1122cfc7fc3eca4eb3026d7fa52711ca49a531d2
James Sturges <james@jsturges.com>
2024-09-02 11:38:49 -0400

    add build version, reorg headers

--- plugins/constants.js
+++ plugins/constants.js
@@ -1,16 +1,19 @@
 
 function plugin() {
 
-  return function(files, metalsmith, done) {
+  return function (files, metalsmith, done) {
+    const gitRef = process.env.GITHUB_SHA
 
     const metadata = metalsmith.metadata()
+    const version = gitRef ? gitRef.substring(0, 8) : undefined
 
     metadata.const = {
       social: {
         ga: {
           id: 'UA-112748787-9'
         }
-      }
+      },
+      version
     }
 
     setImmediate(done)
